6143070c0feb5244cdf4fb4f957b9ce1bee730a8
Krishnaraj286g <krishnaraj286@gmail.com>
2025-05-21 13:08:08 +0530

    .

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router();
-const bcrypt = require("bcriptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
